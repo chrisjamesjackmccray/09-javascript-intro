510dee90563f59f711f8747764e410e7fb3dee95
James McCray <chrisjamesjackmccray@gmail.com>
2016-03-14 14:45:39 -0500

    Assignment Commit

---
+++ index.js
@@ -0,0 +1,78 @@
+var shoe;
+var socks;
+var shirt;
+var pants;
+var hat;
+
+var bike = 2;
+var wheel = 4;
+var skateboard = 6;
+var tv = 8;
+var internet = 10;
+
+
+
+var bike = "hello";
+var wheel = "goodbye";
+var skateboard = "greetings";
+var tv = "partings";
+var internet = "reunion";
+
+
+
+
+
+var game = "basketball";
+
+switch (game) {
+  case "basketball":
+
+    break;
+
+  case "soccer";
+
+  break;
+
+  case "football";
+
+  break;
+
+  default:
+
+  console.log("On the bench")
+}
+
+var bike = true;
+var wheel = false;
+var skateboard = true;
+var tv = false;
+var internet = true;
+
+
+
+// var denied = false, age = 14;
+// age >= 16 denied = true;
+
+
+
+var denied = true;
+var age = 14;
+console.log ("true")
+
+if (age >= 16) {
+  denied = false;
+  console.log ("false")
+}
+
+
+var a = 3;
+var b = 2;
+
+var total = a + b;
+
+
+
+// var numbers = ["4", "8", "12"];
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
